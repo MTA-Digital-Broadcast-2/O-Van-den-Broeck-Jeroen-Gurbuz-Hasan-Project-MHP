44eab0c9a1e68cff4be8cb57d181faa5b243fa6c
jeroenjvdb <jeroen_vdb1@hotmail.com>
2015-03-01 15:42:57 +0100

    initialiseren project

---
+++ opdracht/2048/src/hellotvxlet/HelloTVXlet.java
@@ -0,0 +1,41 @@
+package hellotvxlet;
+
+//import 
+import javax.tv.xlet.*;
+import org.havi.ui.*;
+import org.dvb.ui.*;
+import java.awt.*;
+
+import java.awt.event.*;
+import org.havi.ui.event.*;
+
+
+public class HelloTVXlet implements Xlet, HActionListener {
+    
+  
+    public HelloTVXlet() {
+        
+    }
+
+    public void initXlet(XletContext context) throws XletStateChangeException {
+      
+      
+    }
+
+    public void startXlet() {
+        
+    }
+
+    public void pauseXlet() {
+     
+    }
+
+    public void destroyXlet(boolean unconditional) {
+     
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        
+    }
+}
